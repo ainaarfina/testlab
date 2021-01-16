3bccce5b34eb9f63dd5b804f919b07b58be9be71
aina arfina <ainaarfina@ymail.com>
2021-01-16 11:39:03 +0800

    Initial commit

---
+++ lib/catalogue.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:testlab/PlanB.dart';
+
+class Catalogue extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalogue"),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            GridView.count(
+              shrinkWrap: true,
+              primary: true,
+              crossAxisCount: 2,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () {},
+                  color: Colors.pink,
+                  shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.red[100], width: 2),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Text("Plan A"),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => PlanB()),
+                    );
+                  },
+                  color: Colors.pink,
+                  shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.red[100], width: 2),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Text("Plan B"),
+                ),
+                RaisedButton(
+                  onPressed: () {},
+                  color: Colors.pink,
+                  shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.red[100], width: 2),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Text("Plan C"),
+                ),
+                RaisedButton(
+                  onPressed: () {},
+                  color: Colors.pink,
+                  shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.red[100], width: 2),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Text("Plan D"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+import 'Login.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.blue), home: LoginPage());
+  }
+}
